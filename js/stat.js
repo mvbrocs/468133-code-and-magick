5383a6ba914a7ae612ee5fef31a5fc3b6c770661
Maksim Belov <mvbrocs@gmail.com>
2018-06-10 12:59:29 +0300

    complete modul2-task1

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,18 @@
+var getMaxValue = function (nums) {
+  var max = nums[0];
+
+  for (var i = 1; i < nums.length; i++) {
+    var num = nums[i];
+
+    if (num > max) {
+      max = num;
+    }
+  }
+
+  return max;
+};
+
+
 var renderStatistics = function (ctx, names, times) {
   ctx.globalCompositeOperation = 'destination-over';
 
@@ -33,30 +48,55 @@ var renderStatistics = function (ctx, names, times) {
 
 
   // Draw chart
+  var maxTime = getMaxValue(times) + 500;
   var chartHeight = 150;
+  var stepHeight = maxTime / chartHeight;
   var columnWidth = 40;
-  var gutterWidth = 50;
-  var marginChartFromText = 40;
+  var gutterWidth = 55;
+  var marginChartFromTitle = 45;
+  var colorColumn = '';
   var colorPlayer = 'rgba(255, 0, 0, 1)';
   var chartCoordinates = [...nextLineTextCoordinates];
-  chartCoordinates[1] += marginChartFromText + chartHeight;
+  var textMarginFromChart = 10;
+  chartCoordinates[1] += marginChartFromTitle + chartHeight;
 
   for (var i = 0; i < times.length; i++) {
     ctx.save();
 
-    var colorColumn = '';
+    // Calc color bar
+    var randomOpacity = Math.floor(Math.random() * 10) / 10;
+
+    if (randomOpacity === 0) {
+      randomOpacity = 0.1;
+    }
 
     if (names[i] === 'Вы') {
       colorColumn = colorPlayer;
     } else {
-      colorColumn = 'rgba(0, 0, 255, ' + (Math.floor(Math.random() * 10) / 10) + ')';
+      colorColumn = 'rgba(0, 0, 255, ' + randomOpacity + ')';
     }
+
+    // Calc height bar
+    var currentTime = times[i];
+    var currentTimeInSeconds = currentTime / 1000;
+    var currentBarHeight = currentTime / stepHeight;
+    var roundCurrentTime = Math.floor(currentTimeInSeconds * 10) / 10;
+    var stringTime = roundCurrentTime + 's.';
+    var currentNameCoordinates = [0, -(chartHeight + textMarginFromChart)];
+    var timeStringCoordinates = [...currentNameCoordinates];
+    timeStringCoordinates[1] += 18;
+    var resString = names[i] + stringTime;
 
     ctx.fillStyle = colorColumn;
     ctx.translate(chartCoordinates[0] + (i * (columnWidth + gutterWidth)), chartCoordinates[1]);
     ctx.scale(1, -1);
-    ctx.fillRect(0, 0, columnWidth, chartHeight);
+    ctx.fillRect(0, 0, columnWidth, currentBarHeight);
 
+    // Draw name
+    ctx.scale(1, -1);
+    ctx.fillStyle = '#000000';
+    ctx.fillText(names[i], ...currentNameCoordinates);
+    ctx.fillText(stringTime, ...timeStringCoordinates);
     ctx.restore();
   }
 };
